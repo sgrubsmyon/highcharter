99b161a0b9b0ec30c4b2e7ddabcd2fd09838ea93
Markus Voge <markus.voge@ea-aw.de>
2018-09-27 23:48:45 +0200

    As suggested on https://deanattali.com/blog/htmlwidgets-tips/, have initialization code run only once, afterwards, simply update the existing plot. This commit still contains code (commented out) for checking whether a full update is needed. It did not work so well because seeing if there were any changes is tricky.

--- inst/htmlwidgets/highchart.js
+++ inst/htmlwidgets/highchart.js
@@ -5,90 +5,149 @@ HTMLWidgets.widget({
   type: 'output',
   
   factory: function(el, width, height) {
+    var initialized = false;
+    /*
+    var old_hc_opts = null;
+    */
+    
     return {
       renderValue: function(x) {
-        
-        if (x.debug) {
-          window.xclone = JSON.parse(JSON.stringify(x));
-          window.elclone = $(el);
-          console.log(el);
-          console.log("hc_opts", x.hc_opts);
-          console.log("theme", x.theme);
-          console.log("conf_opts", x.conf_opts);
-        }
- 
-        if (x.fonts !== undefined) {
-          x.fonts = ((typeof(x.fonts) == "string") ? [x.fonts] : x.fonts);
-          x.fonts.forEach(function(s){
-            /* http://stackoverflow.com/questions/4724606 */
-            var urlfont = 'https://fonts.googleapis.com/css?family=' + s;
-            if (!$("link[href='" + urlfont + "']").length) {
-              $('<link href="' + urlfont + '" rel="stylesheet" type="text/css">').appendTo("head");
-            }
-          });
-        }
-        
-        ResetHighchartsOptions();
-        
-        if (x.theme !== null) {
-          if (x.debug) console.log("adding THEME");
-          Highcharts.setOptions(x.theme);
-        }
-        
-        if ((x.theme && x.theme.chart.divBackgroundImage !== undefined) |
-             (x.hc_opts.chart  && x.hc_opts.chart.divBackgroundImage !== undefined)) {
-          if (x.debug) console.log("adding BackgroundImage");     
-          var bkgrnd = x.theme.chart.divBackgroundImage || x.hc_opts.chart.divBackgroundImage;
-          Highcharts.wrap(Highcharts.Chart.prototype, "getContainer", function (proceed) {
-            proceed.call(this);
-            $("#" + el.id).css("background-image", "url(" + bkgrnd + ")");
-            $("#" + el.id).css("-webkit-background-size", "cover");
-            $("#" + el.id).css("-moz-background-size", "cover");
-            $("#" + el.id).css("-o-background-size", "cover");
-            $("#" + el.id).css("background-size", "cover");
-          });
-        }
-        
-        Highcharts.setOptions(x.conf_opts);
-        
-        if (x.type == "chart") {
-          if (x.debug) console.log("charting CHART");
-          $("#" + el.id).highcharts(x.hc_opts);
-        } else if (x.type == "stock") {
-          if (x.debug) console.log("charting STOCK");
-          $("#" + el.id).highcharts('StockChart', x.hc_opts);  
-        } else if (x.type == "map"){
-          if (x.debug) console.log("charting MAP");
-          x.hc_opts.series = x.hc_opts.series.map(function(e){
-            if (e.geojson === true) {
-              if (x.debug) console.log("geojson\n\t", e.type, "\n\t", typeof(e.series));
-              e.data = Highcharts.geojson(e.data, e.type);
-            }
-            return e;
-          });
-          $("#" + el.id).highcharts('Map', x.hc_opts); 
-          if (x.hc_opts.mapNavigation !== undefined && x.hc_opts.mapNavigation.enabled === true){
-            /* if have navigation option and enabled true: http://stackoverflow.com/questions/7600454 */
-            $("#" + el.id).bind( 'mousewheel DOMMouseScroll', function ( e ) {
-              var e0 = e.originalEvent,
-              delta = e0.wheelDelta || -e0.detail;
-              this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
-              e.preventDefault();
+        if (!initialized) {
+          initialized = true;
+          
+          /*
+          // Create copy of object that may be modified w/o changing original object (not by reference)
+          old_hc_opts = Object.assign({}, x.hc_opts);
+          // Remove series, chart.marginBottom and chart.spacingBottom from the object
+          deleteIrrelevantKeys(old_hc_opts);
+          */
+          
+          // Code that shall run only once, every other run will simply update the data without a full redraw
+          if (x.debug) {
+            window.xclone = JSON.parse(JSON.stringify(x));
+            window.elclone = $(el);
+            console.log(el);
+            console.log("hc_opts", x.hc_opts);
+            console.log("theme", x.theme);
+            console.log("conf_opts", x.conf_opts);
+          }
+   
+          if (x.fonts !== undefined) {
+            x.fonts = ((typeof(x.fonts) == "string") ? [x.fonts] : x.fonts);
+            x.fonts.forEach(function(s){
+              /* http://stackoverflow.com/questions/4724606 */
+              var urlfont = 'https://fonts.googleapis.com/css?family=' + s;
+              if (!$("link[href='" + urlfont + "']").length) {
+                $('<link href="' + urlfont + '" rel="stylesheet" type="text/css">').appendTo("head");
+              }
             });
           }
-        }
-        
-        if (x.hc_opts.motion !== undefined) {
-          $("#" + el.id).css({"position" : "relative" });
-          if (x.debug) console.log("setting MOTION options");
-          var pc = $($("#" + el.id).find("#play-controls")[0]);
-          var ct = x.theme.chart;
           
-          if (ct.backgroundColor !== undefined) $(pc.find("#play-pause-button")[0]).css({backgroundColor : x.theme.chart.backgroundColor});
-      if (ct.style !== undefined) $(pc.find("#play-output")[0]).css(x.theme.chart.style);
-      if (ct.style !== undefined && ct.style.color !== undefined) $(pc.find("#play-pause-button")[0]).css({color : x.theme.chart.style.color});
+          ResetHighchartsOptions();
+          
+          if (x.theme !== null) {
+            if (x.debug) console.log("adding THEME");
+            Highcharts.setOptions(x.theme);
+          }
+          
+          if ((x.theme && x.theme.chart.divBackgroundImage !== undefined) |
+               (x.hc_opts.chart  && x.hc_opts.chart.divBackgroundImage !== undefined)) {
+            if (x.debug) console.log("adding BackgroundImage");     
+            var bkgrnd = x.theme.chart.divBackgroundImage || x.hc_opts.chart.divBackgroundImage;
+            Highcharts.wrap(Highcharts.Chart.prototype, "getContainer", function (proceed) {
+              proceed.call(this);
+              $("#" + el.id).css("background-image", "url(" + bkgrnd + ")");
+              $("#" + el.id).css("-webkit-background-size", "cover");
+              $("#" + el.id).css("-moz-background-size", "cover");
+              $("#" + el.id).css("-o-background-size", "cover");
+              $("#" + el.id).css("background-size", "cover");
+            });
+          }
+          
+          Highcharts.setOptions(x.conf_opts);
+          
+          if (x.type == "chart") {
+            if (x.debug) console.log("charting CHART");
+            $("#" + el.id).highcharts(x.hc_opts);
+          } else if (x.type == "stock") {
+            if (x.debug) console.log("charting STOCK");
+            $("#" + el.id).highcharts('StockChart', x.hc_opts);
+          } else if (x.type == "map"){
+            if (x.debug) console.log("charting MAP");
+            x.hc_opts.series = x.hc_opts.series.map(function(e){
+              if (e.geojson === true) {
+                if (x.debug) console.log("geojson\n\t", e.type, "\n\t", typeof(e.series));
+                e.data = Highcharts.geojson(e.data, e.type);
+              }
+              return e;
+            });
+            $("#" + el.id).highcharts('Map', x.hc_opts); 
+            if (x.hc_opts.mapNavigation !== undefined && x.hc_opts.mapNavigation.enabled === true){
+              /* if have navigation option and enabled true: http://stackoverflow.com/questions/7600454 */
+              $("#" + el.id).bind( 'mousewheel DOMMouseScroll', function ( e ) {
+                var e0 = e.originalEvent,
+                delta = e0.wheelDelta || -e0.detail;
+                this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
+                e.preventDefault();
+              });
+            }
+          }
+          
+          if (x.hc_opts.motion !== undefined) {
+            $("#" + el.id).css({"position" : "relative" });
+            if (x.debug) console.log("setting MOTION options");
+            var pc = $($("#" + el.id).find("#play-controls")[0]);
+            var ct = x.theme.chart;
+            
+            if (ct.backgroundColor !== undefined) $(pc.find("#play-pause-button")[0]).css({backgroundColor : x.theme.chart.backgroundColor});
+        if (ct.style !== undefined) $(pc.find("#play-output")[0]).css(x.theme.chart.style);
+        if (ct.style !== undefined && ct.style.color !== undefined) $(pc.find("#play-pause-button")[0]).css({color : x.theme.chart.style.color});
+          }
+        } else {
+          // Code that runs from the second run on
+          
+          // Check if any option besides the series options was changed. In that
+          // case, it's unfortunately necessary to update the entire chart (slower)
+          
+          // Create copy of object that may be modified w/o changing original object
+          var new_hc_opts = Object.assign({}, x.hc_opts);
+          // Delete series:
+          delete new_hc_opts.series;
+          
+          /*
+          // Remove series, chart.marginBottom and chart.spacingBottom from the object
+          deleteIrrelevantKeys(new_hc_opts);
+          console.log(objectEqual(old_hc_opts, new_hc_opts));
+          console.log(old_hc_opts);
+          console.log(new_hc_opts);
+          
+          if (!objectEqual(old_hc_opts, new_hc_opts)) {
+          */
+            // Update the entire chart (except the series) for changes in title etc.
+            $("#" + el.id).highcharts().update(new_hc_opts);
+          /*
+            // Set new hc_opts as the new old
+            old_hc_opts = Object.assign({}, new_hc_opts);
+          }
+          */
+          
+          // Update the series without a full redraw
+          var old_s_length = $("#" + el.id).highcharts().series.length;
+          var new_s_length = x.hc_opts.series.length;
+          var i = 0;
+          x.hc_opts.series.forEach(function(s) {
+            if (i < old_s_length) {
+              $("#" + el.id).highcharts().series[i].update(s);
+            } else {
+              $("#" + el.id).highcharts().addSeries(s);
+            }
+            i++;
+          });
+          // remove any series that might not be desired any more
+          for (i = new_s_length; i < old_s_length; i++) {
+            $("#" + el.id).highcharts().series[i].remove();
+          }
         }
-        
       },
       
       resize: function(width, height) {
@@ -104,3 +163,32 @@ HTMLWidgets.widget({
   }
 
 });
+
+/*
+function deleteIrrelevantKeys(obj) {
+  delete obj.series;
+  delete obj.chart.marginBottom;
+  delete obj.chart.spacingBottom;
+}
+
+function keysEqual(keys1, keys2) {
+  if (keys1.length != keys2.length) return false;
+  for (var i = 0; i < keys1.length; i++) {
+    if (keys1[i] != keys2[i]) return false;
+  }
+  return true;
+}
+          
+function objectEqual(obj1, obj2) {
+  if (!keysEqual(Object.keys(obj1), Object.keys(obj2))) return false;
+  for (let k of Object.keys(obj1)) {
+    if (typeof obj1[k] != typeof obj2[k]) return false;
+    if (obj1[k] === null || typeof obj1[k] != "object") {
+      if (obj1[k] != obj2[k]) {console.log(k); return false;}
+    } else {
+      if (!objectEqual(obj1[k], obj2[k])) {console.log(k); return false;}
+    }    
+  }
+  return true;
+}
+*/
